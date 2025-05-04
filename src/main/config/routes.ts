64823af73884d7396b101c4fc82840acb0e912c1
Larissa <larisousa2401@hotmail.com>
2025-05-04 20:12:41 -0300

    Salvando imagens no cloudflare

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -3,6 +3,7 @@ import { pingRoutes } from '@main/routes'
 import { filmeRoutes } from '@main/modules/filmes/routes'
 import { autenticacaoRoutes } from '../modules/autenticacao/routes'
 import { verificaAutenticacao } from '../infra/middlewares'
+import { uploadRoutes } from '../modules/filmes/routes/upload.routes'
 
 export default function setupRoutes(app: Express) {
   const router = Router()
@@ -14,4 +15,5 @@ export default function setupRoutes(app: Express) {
   router.use(verificaAutenticacao);
 
   filmeRoutes(router)
+  uploadRoutes(router)
 }
---
+++ src/main/modules/filmes/controllers/upload.controller.ts
@@ -0,0 +1,24 @@
+import {
+    created,
+    HttpResponse,
+    badRequest,
+} from '@main/infra/http'
+import { UploadService } from '../services/upload.service'
+
+export class UploadController {
+    private uploadService: UploadService
+
+    constructor(uploadService: UploadService) {
+        this.uploadService = uploadService
+    }
+
+    async upload(file: Express.Multer.File | undefined): Promise<HttpResponse> {
+        if (!file) {
+            return badRequest(new Error('Arquivo não encontrado'))
+        }
+
+        const url = await this.uploadService.enviarParaR2(file)
+
+        return created(url);
+    }
+}
